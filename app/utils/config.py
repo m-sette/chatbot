5825905bb11685305236d25956dbc711d408cd4d
Matteo <msette8@gmail.com>
2023-11-09 18:13:40 +0100

    Modify env var import to prevent type None

--- app/utils/config.py
+++ app/utils/config.py
@@ -4,10 +4,10 @@ import weaviate
 
 load_dotenv('.env') 
 
-WEAVIATE_URL = os.environ.get("WEAVIATE_URL")
-WEAVIATE_API_KEY = os.environ.get("WEAVIATE_API_KEY")
-HUGGINGFACE_API = os.environ.get("HUGGINGFACE_API_KEY")
-BOOKS_URL = os.getenv("BOOKS_URL")
+WEAVIATE_URL = os.environ.get("WEAVIATE_URL", "")
+WEAVIATE_API_KEY = os.environ.get("WEAVIATE_API_KEY", "")
+HUGGINGFACE_API = os.environ.get("HUGGINGFACE_API_KEY", "")
+BOOKS_URL = os.getenv("BOOKS_URL", "")
 
 
 config = {
