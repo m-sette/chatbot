73bd83892f89dff55ee779617e522e4904996ebc
Matteo <msette8@gmail.com>
2023-11-15 21:08:19 +0100

    Add slack socket connection, Add multiprocessing for slack_bolt & FastAPI

--- app/services/app.py
+++ app/services/app.py
@@ -1,13 +1,19 @@
+import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+load_dotenv('.env') 
 
 
-
-
-SLACK_BOT_TOKEN=""
-SLACK_APP_TOKEN=""
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
 
 app = App(token=SLACK_BOT_TOKEN)
 
-if __name__ == "__main__":
-    SocketModeHandler(app, SLACK_APP_TOKEN).start()
+@app.message("hello")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
+
+def run_slack_app():
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from app.routes import question
+from multiprocessing import Process
+from app.services.app import run_slack_app
 
 app = FastAPI()
 
 # handle all routes defined in the '/question' router
 app.include_router(question.router)
 
-if __name__ == "__main__":
+def run_uvicorn_app():
     import uvicorn
     uvicorn.run("main:app", reload=True)
+
+if __name__ == "__main__":
+    api_thread = Process(target=run_uvicorn_app)
+    api_thread.start()
+
+    run_slack_app()
